4cc1debd14d356accc5ea85943347753798cc43e
potapovDim <potapovd3012@gmail.com>
2019-06-17 18:40:47 +0300

    initial specs for sync version

---
+++ __specs__/sync.spec.js
@@ -0,0 +1,22 @@
+const {expect} = require('chai')
+const fs = require('fs')
+const {createTempFile} = require('../lib/sync')
+
+describe('Sync ', () => {
+  it('create temp file defaults and remove', () => {
+    const {remove, filepath} = createTempFile('a')
+    expect(typeof remove).to.eql('function')
+    expect(typeof filepath).to.eql('string')
+    expect(fs.existsSync(filepath)).to.eql(true)
+    remove()
+    expect(fs.existsSync(filepath)).to.eql(false)
+  })
+
+  it('create temp file defaults not valid data', () => {
+    try {
+      createTempFile()
+    } catch({message}) {
+      expect(error.message).to.include('Required argument type is string, got: undefined')
+    }
+  })
+})
