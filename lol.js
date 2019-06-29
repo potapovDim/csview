ce7b62f458ebabc6502181505ab93c05bf47ecab
potapovDim <potapovd3012@gmail.com>
2019-06-29 09:33:08 +0300

    start async version

--- lol.js
+++ lol.js
@@ -1,3 +1,15 @@
-const path = require('path')
+const fs = require('fs')
 
-console.log(path.resolve('dsjalfjlkaslnc,mnx,n,mnc,xzn\\das,das', __dirname))
+const _stream = fs.createReadStream('/Users/dpot/Documents/csview/misc/misc.first.csv')
+
+_stream.on('open', () => {
+  console.log('data')
+})
+
+_stream.on('data', (data) => {
+  // console.log('____________________________ start')
+  // console.log('____________________________')
+  // console.log(data.toString('utf8'))
+  // console.log('____________________________')
+  // console.log('____________________________ end')
+})
